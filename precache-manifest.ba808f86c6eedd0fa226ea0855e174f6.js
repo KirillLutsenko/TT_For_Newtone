8345c8b289aa2020fb1f983bced1ef9d9887ab1e
KirillLutsenko <mnrovski68@gmail.com>
2021-01-29 11:36:54 +0200

    Updates

---
+++ precache-manifest.ba808f86c6eedd0fa226ea0855e174f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72f68983d1e8714319ab987eb2464a67",
+    "url": "/TT_For_Newtone/index.html"
+  },
+  {
+    "revision": "5147a2156b07c577a7c1",
+    "url": "/TT_For_Newtone/static/css/main.a0caafa8.chunk.css"
+  },
+  {
+    "revision": "f0c66d7e0e0cbe5942b0",
+    "url": "/TT_For_Newtone/static/js/2.3ed178a2.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/TT_For_Newtone/static/js/2.3ed178a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5147a2156b07c577a7c1",
+    "url": "/TT_For_Newtone/static/js/main.90050ceb.chunk.js"
+  },
+  {
+    "revision": "6ce171acfd1545b6141f",
+    "url": "/TT_For_Newtone/static/js/runtime-main.90bfa7c3.js"
+  }
+]);
